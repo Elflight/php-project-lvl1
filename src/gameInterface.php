60b9830b62abb5ff1a8116f16b13147234c3c88e
elf <elflight@yandex.ru>
2020-07-10 20:34:00 +0300

    refactoring

--- src/gameInterface.php
+++ src/gameInterface.php
@@ -2,14 +2,14 @@
 
 namespace Brain\Game\GameInterface;
 
-use function cli\line;
-use function cli\prompt;
-
 use Brain\Games\Calculator;
 use Brain\Games\Even;
 use Brain\Games\Gcd;
 use Brain\Games\Progression;
 use Brain\Games\Prime;
+
+use function cli\line;
+use function cli\prompt;
 
 function run($gameName)
 {
@@ -26,10 +26,10 @@ function run($gameName)
 
     $noErrors = true;
     for ($i = 0; $i < ROUNDSCOUNT; $i++) {
-        $arGameRoundResult = call_user_func('Brain\Games\\' . $gameName . '\gameRound');
+        $arGameRound = call_user_func('Brain\Games\\' . $gameName . '\gameRound');
 
-        if ($arGameRoundResult['answer'] != $arGameRoundResult['solution']) {
-            line("'{$arGameRoundResult['answer']}' is wrong answer ;(. Correct answer was '{$arGameRoundResult['solution']}'.");
+        if ($arGameRound['answer'] != $arGameRound['solution']) {
+            line("'{$arGameRound['answer']}' is wrong answer ;(. Correct answer was '{$arGameRound['solution']}'.");
             line("Let's try again, {$userName}!");
             $noErrors = false;
             break;
