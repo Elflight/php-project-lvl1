fa15f4e85a3e348dfd42fa8d376b15709bb19948
elf <elflight@yandex.ru>
2020-07-13 21:52:07 +0300

    refactoring

--- src/gameInterface.php
+++ src/gameInterface.php
@@ -2,19 +2,13 @@
 
 namespace Brain\Game\GameInterface;
 
-use Brain\Games\Calculator;
-use Brain\Games\Even;
-use Brain\Games\Gcd;
-use Brain\Games\Progression;
-use Brain\Games\Prime;
-
 use function cli\line;
 use function cli\prompt;
 
+const ROUNDS_COUNT = 3;
+
 function run($gameName)
 {
-    define("ROUNDSCOUNT", 3);
-
     $rules = call_user_func('Brain\Games\\' . $gameName . '\getRules');
     line('Welcome to the Brain Games!');
     line($rules);
@@ -25,7 +19,7 @@ function run($gameName)
     line();
 
     $noErrors = true;
-    for ($i = 0; $i < ROUNDSCOUNT; $i++) {
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $arGameRound = call_user_func('Brain\Games\\' . $gameName . '\gameRound');
 
         if ($arGameRound['answer'] != $arGameRound['solution']) {
